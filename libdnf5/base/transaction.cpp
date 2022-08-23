785f28f5d0253d9987ab0b63507bf789664d5020
Reiner Hauser <Reiner.Hauser@cern.ch>
2022-08-23 18:03:25 +0200

    Use user runtime dir for rpm transaction lock

--- libdnf5/base/transaction.cpp
+++ libdnf5/base/transaction.cpp
@@ -36,6 +36,7 @@
 #include "transaction_module_impl.hpp"
 #include "transaction_package_impl.hpp"
 #include "utils/string.hpp"
+#include "utils/xdg.hpp"
 
 #include "libdnf5/base/active_transaction_info.hpp"
 #include "libdnf5/base/base.hpp"
