e2feb0654c24d294adac7c7e121d3f9b5f85e2c5
Reiner Hauser <Reiner.Hauser@cern.ch>
2023-03-08 09:11:36 +0000

    Adjust to changed APIs

--- libdnf5/base/transaction.cpp
+++ libdnf5/base/transaction.cpp
@@ -37,7 +37,6 @@
 #include "transaction_package_impl.hpp"
 #include "utils/on_scope_exit.hpp"
 #include "utils/string.hpp"
-#include "utils/xdg.hpp"
 
 #include "libdnf5/base/active_transaction_info.hpp"
 #include "libdnf5/base/base.hpp"
@@ -1097,8 +1096,11 @@ Transaction::TransactionRunResult Transaction::Impl::_run(
     info.set_start_time(std::chrono::system_clock::to_time_t(std::chrono::system_clock::now()));
 
     // acquire the lock
+
     std::filesystem::path lock_file_path = config.get_installroot_option().get_value();
-    lock_file_path /= std::filesystem::path(libdnf5::TRANSACTION_LOCK_FILEPATH).relative_path();
+    lock_file_path /= libdnf::xdg::get_user_runtime_dir();
+    lock_file_path /= "rpmtransaction.lock";
+
     std::filesystem::create_directories(lock_file_path.parent_path());
 
     auto logger = base->get_logger().get();
